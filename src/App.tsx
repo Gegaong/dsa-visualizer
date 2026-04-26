0dd5bc50526490ab278b0397803987e8fd914502
Gegaong <gormo22@freeuni.edu.ge>
2026-04-27 00:04:39 +0400

    setup react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>DSA Visualizer Sandbox</h1>
+      <p>My first custom React component</p>
+    </div>
+  )
+}
+
+export default App
